87be55e61ffa4eeeff54aee31871ef9ad0070553
Johny Cruz <johnycruz00@gmail.com>
2020-03-26 03:22:00 -0300

    Conceitos de Imutabilidade e Estado
    Exemplos mostrando como os conceitos de imutabilidade e estado funcionam no  React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react'; //O useState deve ser importado do React para poder atualizar algum objeto da página sem ferir o principio da imutabilidade
+import Header from './Header';
+
+
 
 function App() {
+  const [counter, setCounter] = useState(0); // inicializo um contador e junto com ele a funcao para atualiza-lo
+  
+  function increment(){ // No react podemos criar uma função dentro da outra
+    setCounter(counter + 1); // Eu não poderia mudar o contador direto, pelo conceito de imutabilidade do React
+  }
+  
   return (
-   <h1>Hello World</h1>
+    //No React nunca podemos ter duas tags juntas que não estejam dentro de outra tag
+    //Quando for chamar uma função dentro de um objeto ela deve ser passada entre {}
+    <div> 
+        <Header>
+          Contador: {counter}
+        </Header>
+        <button onClick={increment}>Incrementa</button>
+   </div>
   );
 }
 

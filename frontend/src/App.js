93aa736bf0e6b484fdba2b7496cc88b5faa48c4e
Johny Cruz <johnycruz00@gmail.com>
2020-03-26 03:38:49 -0300

    criação do CSS basico da aplicação
    Removi o que foi feito até aqui e todas as anotações iniciais, após isso inclui as imagens que serão utilizadas no projeto e inclui o css básico de todas as páginas, além disso inclui os arquivos para a página de logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,12 @@
-import React, {useState} from 'react'; //O useState deve ser importado do React para poder atualizar algum objeto da página sem ferir o principio da imutabilidade
-import Header from './Header';
+import React from 'react'; //O useState deve ser importado do React para poder atualizar algum objeto da página sem ferir o principio da imutabilidade  
+import Logon from './pages/Logon';
 
-
+import './global.css'
 
 function App() {
-  const [counter, setCounter] = useState(0); // inicializo um contador e junto com ele a funcao para atualiza-lo
-  
-  function increment(){ // No react podemos criar uma função dentro da outra
-    setCounter(counter + 1); // Eu não poderia mudar o contador direto, pelo conceito de imutabilidade do React
-  }
   
   return (
-    //No React nunca podemos ter duas tags juntas que não estejam dentro de outra tag
-    //Quando for chamar uma função dentro de um objeto ela deve ser passada entre {}
-    <div> 
-        <Header>
-          Contador: {counter}
-        </Header>
-        <button onClick={increment}>Incrementa</button>
-   </div>
+    <Logon/>
   );
 }
 

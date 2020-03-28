eed63d3aba5c809d042d8efc6203781214e91e16
Johny Cruz <johnycruz00@gmail.com>
2020-03-28 18:44:19 -0300

    mais detalhes
    finalizei a parte de enviar dados de uma tela para outra

--- mobile/src/pages/detail/index.js
+++ mobile/src/pages/detail/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {View,TouchableOpacity , Text, Image, Linking} from 'react-native';
 import {Feather} from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native'; //useRoute é usado para pegar informações da página atual da nossa aplicação
 import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Incidents(){
+
     const navigation = useNavigation();
-    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$ 120,00';
+    const route = useRoute();
+
+    const incident = route.params.incident;
+    const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-br',{style:'currency', currency:'BRL'}).format(incident.value) }`;
     
     function navigateBack(){
         navigation.goBack();
@@ -17,14 +21,14 @@ export default function Incidents(){
 
     function sendMail(){
         MailComposer.composeAsync({
-            subject: 'Herói do caso: Cadelinha atropelada',
-            recipients: ['johnywnc@hotmail.com'],
+            subject: `Herói do caso: ${incident.title}`,
+            recipients: [incident.email],
             body: message,
         });
     }
 
     function sendWhatsapp(){
-        Linking.openURL(`whatsapp://send?phone=5519993086497&text=${message}`)
+        Linking.openURL(`whatsapp://send?phone=5519993086497&text=${message}`)//TODO: aqui eu devo substituir o numero do whatsapp pelo incident.whatsapp
     }
     return (
         <View style={styles.container}>
@@ -37,13 +41,13 @@ export default function Incidents(){
 
             <View style={styles.incident}>
                 <Text style={styles.incidentProperty, {marginTop: 0}}>ONG:</Text>
-                <Text style={styles.incidentValue}>APAD</Text>
+    <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
                 <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
 
                 <Text style={styles.incidentProperty}>Valor:</Text>
-                <Text style={styles.incidentValue}>R$ 120,00</Text>
+                <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-br',{style:'currency', currency:'BRL'}).format(incident.value) }</Text>
             </View>
 
             <View style={styles.contactBox}>
